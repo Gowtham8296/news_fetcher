514fe9d0f74e029f217832d8669951fd94ceb608
Gowtham GR <recro.gowtham@bigbasket.com>
2025-10-29 22:57:32 +0530

    updated code

--- src/test/java/com/example/selenium/GoogleSearchTest.java
+++ src/test/java/com/example/selenium/GoogleSearchTest.java
@@ -6,8 +6,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.time.Duration;
 import java.util.List;
 
 import static utilities.utilities.*;
@@ -48,8 +51,9 @@ public class GoogleSearchTest {
         click(driver, "//*[@name='q']");
         sendValueToSearchField(driver,"times of india","//*[@name='q']");
 
-        Wait(driver,10,"//*[contains(text(),'Breaking News')]");
-        click(driver,"//*[contains(text(),'Breaking News')]");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Breaking News')]")));
+
 
         // After line 42
         List<WebElement> maybeLaterBtn = driver.findElements(By.xpath("//*[@class='boxBtnWrap']//*[contains(text(),'Maybe later')]"));
